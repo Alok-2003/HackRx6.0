5e19f5797ee9ec789b935b982815dd3452d377dd
Alok-2003 <alok953280@gmail.com>
2025-08-02 15:42:13 +0530

    refactor: simplify vercel config and update PDF.js imports for better compatibility

--- utils.js
+++ utils.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
-import { getDocument } from 'pdfjs-dist';
+import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist/legacy/build/pdf.mjs';
+
+// Optional: If you want to set workerSrc manually, though it's not always necessary in Node.js
+// GlobalWorkerOptions.workerSrc = './node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs';
 
 export async function fetchAndExtractPDF(url) {
   const response = await axios.get(url, { responseType: "arraybuffer" });
   const data = new Uint8Array(response.data);
 
-  const loadingTask = getDocument({ data });
+  const loadingTask = getDocument({
+    data,
+    standardFontDataUrl: 'node_modules/pdfjs-dist/standard_fonts/',
+  });
+
   const pdf = await loadingTask.promise;
 
   let text = '';
